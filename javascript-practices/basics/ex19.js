ac4010bf260ca36d326be16febff0121971bcb11
kimgip <gippeum0102@gmail.com>
2024-07-04 16:57:50 +0900

    javascript basic: function, prototype, closure, 전역객체

---
+++ javascript-practices/basics/ex19.js
@@ -0,0 +1,12 @@
+/*
+
+전역 객체(global object)
+node: global
+browser: window
+
+*/
+global.n = '둘리'; // 전역객체에 존재
+console.log(global.n, n);
+
+var email = 'dooly@gmail.com'; // 전역범위에 존재
+console.log(global.email, email);
