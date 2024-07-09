3036f86e22444481b9d8bb934bc9612b12332599
kimgip <gippeum0102@gmail.com>
2024-07-09 17:16:31 +0900

    js es6: `${}`, default parameter

---
+++ javascript-practices/es6/ex03.js
@@ -0,0 +1,17 @@
+/**
+ * Template Literal
+ */
+
+const no = 10;
+const name = '둘리';
+const email = 'dooly@gmail.com';
+
+// ex1
+console.log('no:'+no+', name:'+name+', email:'+email);
+console.log(`no:${no}, name:${name}, email:${email}`);
+
+// ex1: 다중행
+console.log('no:10\nname:둘리\nemail:dooly@gmail.com');
+console.log(`no:10
+name:둘리
+email:dooly@gmail.com`);
